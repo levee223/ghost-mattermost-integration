4872379ee6d7b5d1c44d79a7390dbf17408077f9
lameducks <lameducks@users.noreply.github.com>
2020-11-26 23:50:22 +0900

    Make easy to read abbreviate search text

--- src/main/java/app/util/Abbreviator.java
+++ src/main/java/app/util/Abbreviator.java
@@ -13,17 +13,27 @@ public final class Abbreviator {
             return text;
         }
 
+        final int maxBackwardLength = maxLength / 10;
+
         int minKeywordIndex = Integer.MAX_VALUE;
+        int abbreviateOffset = minKeywordIndex;
         for (final String keyword : keywords) {
-            final int keywordIndex = text.indexOf(keyword);
+            final int keywordIndex = StringUtils.indexOfIgnoreCase(text, keyword);
             if (keywordIndex != -1) {
                 minKeywordIndex = Math.min(minKeywordIndex, keywordIndex);
+                for (int i = Math.max(minKeywordIndex - maxBackwardLength, 0); i < minKeywordIndex; i++) {
+                    final char ch = text.charAt(i);
+                    if (Character.isWhitespace(ch) || ".,!?;)/&]}\"'、。．，！？；）／＆］｝”’」】』〉〕､｡｣".indexOf(ch) != -1) {
+                        abbreviateOffset = i + 1;
+                        break;
+                    }
+                }
             }
         }
         if (minKeywordIndex == Integer.MAX_VALUE) {
             return StringUtils.abbreviate(text, maxLength);
         }
-        return StringUtils.abbreviate(text, minKeywordIndex, maxLength);
+        return StringUtils.abbreviate(text, Math.min(abbreviateOffset, minKeywordIndex), maxLength);
     }
 
 }
